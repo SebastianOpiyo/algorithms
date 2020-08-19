b9cf91852445269c7777fc3fbb215ffa198212a9
SebastianOpiyo <sebamopale@gmail.com>
2020-08-20 00:56:34 +0300

    Improved the solution: taking care of exceptions.

--- codility_challanges/BinaryGap.py
+++ codility_challanges/BinaryGap.py
@@ -1,32 +1,40 @@
 # def Solution(N):
 # Convert the +v int into binary
 # zero_counter variable
-# Check the max num of zeros within 1's by use of a loop
+# Check the max num of zeros within 1's by splitting the binary
 # return the max num if existing otherwise return 0
 
+# Solution 1: Best solution.
 def solution(n):
     # checks for the correct value of n
+    bin_limit = 2147483647
     if not isinstance(n, int):
         raise TypeError("Only positive integer values accepted!")
 
-    if n < 1 and (n > 2, 147, 483, 647):
+    if 1 > n:
         raise ValueError("Only positive values less than 2,147,483,647 are accepted!")
 
-    binary_num = f'{n:b}'
-    print(binary_num)
+    if n > bin_limit:
+        raise ValueError("Integer beyond limit!")
 
-    # final_max_value = None
-    # temp_max = None
-    # for bin_0 in binary_num:
-    #     if bin_0 == 0:
-    #         pass
-    # if final_max_value is not None:
-    #     return final_max_value
-    # else:
-    #     return 0
+    # convert n to binary string
+    # and strip off any hanging zeros at the end of it.
+    binary_num = f'{n:b}'.strip('0')
 
-    # Improved the solution below.
-    return len(max(format(n, 'b').strip('0').split('1')))
+    # get the list of zeros
+    list_of_zeros = binary_num.split('1')
+    # Get the len of max zeros
+    max_value = len(max(list_of_zeros))
+    # take care of lack of zero
+    if max_value == '':
+        return 0
+    else:
+        return max_value
 
 
-print(solution(1053))
+# # 2. Condensed and less readable.
+# def solution(n):
+#     return len(max(format(n, 'b').strip('0').split('1')))
+
+number = 2147483647
+print(solution(number))
