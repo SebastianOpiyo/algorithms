3f0bb8a61ed6062794dd5e63e7c6770105d11ecf
SebastianOpiyo <sebamopale@gmail.com>
2020-08-20 03:29:35 +0300

    Fix empty array return value.

--- codility_challanges/CyclicRotation.py
+++ codility_challanges/CyclicRotation.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 # Author: Sebastian Opiyo
-# Date Created: Aug 19, 2020
+# Date Created: Aug 20, 2020
 # Date Modified: Aug 20, 2020
 # Description: Algorithm challenges Lesson 1: Arrays.
 """
@@ -15,13 +15,16 @@ def solution(a, k):
 
 def solution(a: list, k: int):
     """
-        Shift index position of integers in cyclic rotation
+        Rotate an array to the right k steps.
         :param a: an array of integers
         :param k: number of times to shift to cycle the list
         :return: an array of reverser ints after k cycles
     """
-    if not a:
-        raise IndexError("Empty List!")
+    # if not a:
+    #     raise IndexError("Empty List!")
+
+    if not len(a):
+        return a
 
     if len(a) > 100:
         raise ValueError("Length beyond required limit!")
