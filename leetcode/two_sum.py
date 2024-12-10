7cb21c64a16893ebf1944a42f8e4380a67ea3d3a
SebastianOpiyo <sebamopale@gmail.com>
2024-12-10 14:22:58 +0300

    Add solution for two-sum, with 61% performance.

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -41,3 +41,39 @@ class Solution:
             else:
                 hashmap[rem] =i
         return []
+
+
+# I coded this solution a while ago, need to do comparison
+class SolutionOne:
+    def twoSum(self, nums: list, target: int) -> list:
+        existing = {}
+        for i in range(len(nums)):
+            required_num = target - nums[i]
+            if required_num in existing:
+                return [i, existing[required_num]]
+            else:
+                existing[nums[i]] = i
+        return []
+    
+    
+# My third Solution, using hashmap
+# The solution beats 53% of the solutions, still needs optimization
+'''
+Algorithm
+We are looking for two elems in the list, when summed equal the target.
+1. Create a hashtable of the elements in the list with element as key and index as value
+2. pick the first elem in the list and sub from target to get rem
+3. check to see of the rem exists in the dictionary and its index is greater than current iteration
+4. if its true return index of the elem
+
+Time Complexity: O(nlog(n))
+'''
+def list_hasmap(lst:list, target:int) -> list:
+    hashmap = {val:i for i, val  in enumerate(lst)}
+    for i in range(len(lst)):
+        rem = target - lst[i]
+        if rem in hashmap and hashmap[rem] != i:
+            return [hashmap[rem], i]
+    return []
+
+print(list_hasmap([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], target=11))
--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 Algorithm
 We are looking for two elems in the list, when summed equal the target.
 1. Create a hashtable of the elements in the list with element as key and index as value
+[Improvement: Create a hashmap on the fly, instead of creating it first.]
 2. pick the first elem in the list and sub from target to get rem
 3. check to see of the rem exists in the dictionary and its index is greater than current iteration
 4. if its true return index of the elem
@@ -10,14 +11,16 @@ We are looking for two elems in the list, when summed equal the target.
 Time Complexity: O(nlog(n))
 '''
 
+
+
 def list_hasmap(lst:list, target:int) -> list:
-    hashmap = {val:i for i, val  in enumerate(lst)}
-    for i in range(len(lst)):
-        rem = target - lst[i]
+    hashmap = {}
+    for i, val in enumerate(lst):
+        rem = target - val
         if rem in hashmap and hashmap[rem] != i:
             return [hashmap[rem], i]
-        else:
-            hashmap[rem] =i
+        # We update the hashmap at this point
+        hashmap[val] = i
     return []
 
-print(list_hasmap([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], target=11))
+print(list_hasmap([3,2,4], target=6))
