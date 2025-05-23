bd5f065e32c418dfb38984a6a9e063111ef75c8d
SebastianOpiyo <sebamopale@gmail.com>
2025-05-23 18:26:42 +0300

    Add second solution.

--- leetcode/palindrome-number.py
+++ leetcode/palindrome-number.py
@@ -46,29 +46,46 @@ class Solution:
     
     
     # Solution 2 (No string conversion)
-    def ispalindrome2(self, x: int) -> bool:
+    def isPalindrome2(self, x: int) -> bool:
+        '''Second best solution.'''
         # Check if a number is negative
-        if x < 0 or (x % 10 and x != 0):
+        
+        if x < 0 or (x % 10==0 and x != 0):
             return False
         
-        return
+        reversed_num = None
+        original_num = x
+        while x > 0:
+            num_digits = x % 10
+            x //= 10
+            reversed_num = (reversed_num or 0) * 10 + num_digits
+        if reversed_num == original_num:
+            return True
+        return False
     
-
+    
+    
+    
 def digits(x: int) -> int:
     """Count the number of digits in an int"""
-    
+    reversed_num = None
+    digits_count = 0
     while x > 0:
         num_digits = x % 10
-        num_digits = x // 10
-        return len(str(num_digits))
-
+        x //= 10
+        digits_count +=1
+    return digits_count
 
 
 if __name__ == "__main__":
-    print(digits(1234567890))
-    # x = 121
+    # print(digits(1234567890))
+    x = 121
+    print(Solution().isPalindrome2(x))
     # print(Solution().isPalindrome(x))
     # x = -121
+    # print(Solution().isPalindrome2(x))
     # print(Solution().isPalindrome(x))
     # x = 10
+    # print(Solution().isPalindrome2(x))
     # print(Solution().isPalindrome(x))
+    
