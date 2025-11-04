20838daca4875485126529919dbc26125d0ea92a
doka <sebamopale@gmail.com>
2025-11-04 09:37:50 +0300

    Add simple recursion implementation

--- patterns/recursion.py
+++ patterns/recursion.py
@@ -1,17 +1,23 @@
+import sys
+
+
+print(sys.setrecursionlimit(3000))
 # Simple Recursive funtion to demonstrate recursion
 
 # TODO: Fix RecursionError: maximum recursion depth exceeded
-def add_numbers(n):
+def add_numbers(n: int) -> int:
     # Set the base case 
-    if n == 1000:
-        return n
+    if n > 200:
+        return f"Number greater than 200 reached:  {str(n)}"
+
     else:
-        return n + add_numbers(n+500)
+        print(f"Adding {str(n)}")
+        return n + add_numbers(n+1)
 
 
 # Factorial of a number using Recursion
 
-def fact(n):
+def fact(n: int) -> int:
     # base case
     if n == 0:
         return 1
@@ -19,5 +25,6 @@ def fact(n):
     return n * fact(n-1)
 
 if __name__ == "__main__":
-    n = int(input("Enter a number to compute factorial: "))
-    print(f"{n}! is {fact(n)}")
+    # n = int(input("Enter a number to compute factorial: "))
+    # print(f"{n}! is {fact(n)}")
+    add_numbers(10)
